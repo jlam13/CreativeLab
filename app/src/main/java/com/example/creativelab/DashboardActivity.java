7e129ded708c85e55735ddab7eff123809bd81fc
jen-lam <46600747+jen-lam@users.noreply.github.com>
2019-04-06 15:29:32 +1100

    Added fragments for Hub, Learn and Profile tabs

--- app/src/main/java/com/example/creativelab/DashboardActivity.java
+++ app/src/main/java/com/example/creativelab/DashboardActivity.java
@@ -3,15 +3,16 @@ package com.example.creativelab;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class DashboardActivity extends MainActivity {
+public class DashboardActivity extends MainActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+/*    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
@@ -30,15 +31,44 @@ public class DashboardActivity extends MainActivity {
             return false;
         }
     };
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+        navigation.setSelectedItemId(R.id.navigation_learn);
+
+        loadFragment(new LearnFragment());
+
     }
 
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch(item.getItemId()) {
+            case R.id.navigation_hub:
+                fragment = new HubFragment();
+                break;
+            case R.id.navigation_learn:
+                fragment = new LearnFragment();
+                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 }
