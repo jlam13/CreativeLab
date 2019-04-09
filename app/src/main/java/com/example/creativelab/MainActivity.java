5d38b6102293910ea7e9833f5f4e9198b1700fd3
Nathan Cheung <nathancheung77@gmail.com>
2019-04-09 13:37:03 +1000

    Changing to if statements

--- app/src/main/java/com/example/creativelab/MainActivity.java
+++ app/src/main/java/com/example/creativelab/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.creativelab;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView Password;
+    private TextView Username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         );
+
         Button createaccountbutton;
         createaccountbutton = (Button) findViewById(R.id.createaccountbutton);
         createaccountbutton.setOnClickListener(new View.OnClickListener(){
