7291fd8bffe3ea78299c6e6da43a185349b7a445
Nathan Cheung <nathancheung77@gmail.com>
2019-04-09 13:30:15 +1000

    Main Activity Changed

--- app/src/main/java/com/example/creativelab/MainActivity.java
+++ app/src/main/java/com/example/creativelab/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.creativelab;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button viewbutton;
+        viewbutton = (Button) findViewById(R.id.loginbutton);
+        viewbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opendashboard();
+            }
+        }
+        );
+        Button createaccountbutton;
+        createaccountbutton = (Button) findViewById(R.id.createaccountbutton);
+        createaccountbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                opensignup();
+            }
+        }
+        );
+    }
+    public void opendashboard() {
+        Intent intent =new Intent(this, DashboardActivity.class);
+        startActivity(intent);
 
     }
-        public void onButtonClick(View view) {
-            Intent intent = new Intent(getBaseContext(), DashboardActivity.class);
-            startActivity(intent);
-        }
+    public void opensignup() {
+        Intent intent =new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+
+    }
 }
