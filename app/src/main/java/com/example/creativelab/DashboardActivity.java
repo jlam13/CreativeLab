160ef82141893dac60037cd64faffc4667eaa7ee
DeviateT <deveater479@gmail.com>
2019-04-09 13:28:07 +1000

    login done

--- app/src/main/java/com/example/creativelab/DashboardActivity.java
+++ app/src/main/java/com/example/creativelab/DashboardActivity.java
@@ -4,11 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class DashboardActivity extends MainActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
