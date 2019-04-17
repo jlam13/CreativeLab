5bd7b25b34f28b3ca3d7f70ee05cc881f15dfff0
jen-lam <46600747+jen-lam@users.noreply.github.com>
2019-04-17 13:30:27 +1000

    Nested recyclerview update

--- app/src/main/java/com/example/creativelab/Learn/HorizontalAdapter.java
+++ app/src/main/java/com/example/creativelab/Learn/HorizontalAdapter.java
@@ -2,6 +2,7 @@ package com.example.creativelab.Learn;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,19 +11,21 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.creativelab.Learn.Lesson.LessonActivity;
+import com.example.creativelab.Learn.Lesson.TestActivity;
 import com.example.creativelab.R;
 
 import java.util.ArrayList;
 
 public class HorizontalAdapter extends RecyclerView.Adapter<HorizontalAdapter.ViewHolder> {
 
-    //private List<Movie> moviesList;
-
     private LearnInformation learnInformation;
     private ArrayList<Learn> learnArrayList;
     private Context mContext;
+    private OnClickListener mOnClickListener;
+    private Learn learn;
 
-    public HorizontalAdapter (Context mContext,ArrayList<Learn> learnArrayList) {
+    public HorizontalAdapter (Context mContext, ArrayList<Learn> learnArrayList) {
         this.learnArrayList = learnArrayList;
         this.mContext = mContext;
     }
@@ -38,14 +41,22 @@ public class HorizontalAdapter extends RecyclerView.Adapter<HorizontalAdapter.Vi
     public void onBindViewHolder(final ViewHolder holder,final int position) {
         final Learn learn = learnArrayList.get(position);
         holder.lessonName.setText(learn.getLessonName());
-        holder.lessonName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(mContext, learn.getLessonName(), Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
+            holder.lessonName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (learn.getLessonId().contains("z")) {
+                        Intent intent = new Intent(mContext, TestActivity.class);
+                        mContext.startActivity(intent);
+                    }
+                    else {
+                        Intent intent = new Intent(mContext, LessonActivity.class);
+                        mContext.startActivity(intent);
+                    }
+                    Toast.makeText(mContext, learn.getLessonName(), Toast.LENGTH_SHORT).show();
+                }
+            });
 
     }
 
@@ -56,6 +67,7 @@ public class HorizontalAdapter extends RecyclerView.Adapter<HorizontalAdapter.Vi
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView lessonName;
+        OnClickListener onClickListener;
 
 
         public ViewHolder(View view) {
@@ -63,5 +75,9 @@ public class HorizontalAdapter extends RecyclerView.Adapter<HorizontalAdapter.Vi
             lessonName = (TextView) view.findViewById(R.id.lessonName);
 
         }
+            }
+
+    public interface OnClickListener {
+        void onClick (int position);
     }
 }
--- app/src/main/java/com/example/creativelab/Learn/LearnFragment.java
+++ app/src/main/java/com/example/creativelab/Learn/LearnFragment.java
@@ -26,32 +26,32 @@ public class LearnFragment extends Fragment {
             "\t\t{\"lessonId\":\"1\",\"lessonName\":\"PS 1\"},\n" +
             "\t\t{\"lessonId\":\"2\",\"lessonName\":\"PS 2\"},\n" +
             "\t\t{\"lessonId\":\"3\",\"lessonName\":\"PS 3\"},\n" +
-            "\t\t{\"lessonId\":\"4\",\"lessonName\":\"PS Test\"}]},\n" +
+            "\t\t{\"lessonId\":\"z4\",\"lessonName\":\"PS Test\"}]},\n" +
             "\t{\"Editor\":\"Illustrator\",\"lessons\":[\n" +
             "\t\t{\"lessonId\":\"5\",\"lessonName\":\"IL 1\"},\n" +
             "\t\t{\"lessonId\":\"6\",\"lessonName\":\"IL 2\"},\n" +
             "\t\t{\"lessonId\":\"7\",\"lessonName\":\"IL 3\"},\n" +
-            "\t\t{\"lessonId\":\"8\",\"lessonName\":\"IL Test\"}]},\n" +
+            "\t\t{\"lessonId\":\"z8\",\"lessonName\":\"IL Test\"}]},\n" +
             "\t{\"Editor\":\"InDesign\",\"lessons\":[\n" +
             "\t\t{\"lessonId\":\"9\",\"lessonName\":\"ID 1\"},\n" +
             "\t\t{\"lessonId\":\"10\",\"lessonName\":\"ID 2\"},\n" +
             "\t\t{\"lessonId\":\"11\",\"lessonName\":\"ID 3\"},\n" +
-            "\t\t{\"lessonId\":\"12\",\"lessonName\":\"ID Test\"}]},\n" +
+            "\t\t{\"lessonId\":\"z12\",\"lessonName\":\"ID Test\"}]},\n" +
             "\t{\"Editor\":\"Lightroom\",\"lessons\":[\n" +
             "\t\t{\"lessonId\":\"13\",\"lessonName\":\"LR 1\"},\n" +
             "\t\t{\"lessonId\":\"14\",\"lessonName\":\"LR 2\"},\n" +
             "\t\t{\"lessonId\":\"15\",\"lessonName\":\"LR 3\"},\n" +
-            "\t\t{\"lessonId\":\"16\",\"lessonName\":\"LR Test\"}]},\n" +
+            "\t\t{\"lessonId\":\"z16\",\"lessonName\":\"LR Test\"}]},\n" +
             "\t{\"Editor\":\"Premiere Pro\",\"lessons\":[\n" +
             "\t\t{\"lessonId\":\"17\",\"lessonName\":\"PP 1\"},\n" +
             "\t\t{\"lessonId\":\"18\",\"lessonName\":\"PP 2\"},\n" +
             "\t\t{\"lessonId\":\"19\",\"lessonName\":\"PP 3\"},\n" +
-            "\t\t{\"lessonId\":\"20\",\"lessonName\":\"PP Test\"}]},\n" +
+            "\t\t{\"lessonId\":\"z20\",\"lessonName\":\"PP Test\"}]},\n" +
             "\t{\"Editor\":\"After Effects\",\"lessons\":[\n" +
             "\t\t{\"lessonId\":\"21\",\"lessonName\":\"AE 1\"},\n" +
             "\t\t{\"lessonId\":\"22\",\"lessonName\":\"AE 2\"},\n" +
             "\t\t{\"lessonId\":\"23\",\"lessonName\":\"AE 3\"},\n" +
-            "\t\t{\"lessonId\":\"24\",\"lessonName\":\"AE Test\"}]}\n" +
+            "\t\t{\"lessonId\":\"z24\",\"lessonName\":\"AE Test\"}]}\n" +
             "\t\t]};\t";
 
     ArrayList<LearnLessons> learnLessonsArrayList;
