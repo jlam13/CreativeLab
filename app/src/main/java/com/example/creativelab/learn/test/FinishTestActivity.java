94d62d9b137e1f4a00e7d5556b2e6d30883e3f28
jen-lam <46600747+jen-lam@users.noreply.github.com>
2019-04-23 22:08:52 +1000

    ArrayList test

--- app/src/main/java/com/example/creativelab/learn/test/FinishTestActivity.java
+++ app/src/main/java/com/example/creativelab/learn/test/FinishTestActivity.java
@@ -1,4 +1,41 @@
 package com.example.creativelab.learn.test;
 
-public class FinishTestActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.creativelab.DashboardActivity;
+import com.example.creativelab.R;
+import com.example.creativelab.learn.LearnFragment;
+
+public class FinishTestActivity extends AppCompatActivity {
+    TextView mGrade, mFinalScore;
+    Button mRetryButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_test);
+
+        mGrade = (TextView) findViewById(R.id.grade);
+        mFinalScore = (TextView) findViewById(R.id.outOf);
+        mRetryButton = (Button) findViewById(R.id.retry);
+
+
+        Bundle bundle = getIntent().getExtras();
+        int score = bundle.getInt("finalScore");
+
+        mFinalScore.setText("You scored " + score + " out of 100");
+        mRetryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), StartTestActivity.class));
+                FinishTestActivity.this.finish();
+            }
+        });
+
+    }
 }
