03bc0209b1f24b7f5c5dce6726be1f6cce28684f
jen-lam <46600747+jen-lam@users.noreply.github.com>
2019-04-26 16:19:54 +1000

    :(

--- app/src/main/java/com/example/creativelab/learn/test/User.java
+++ app/src/main/java/com/example/creativelab/learn/test/User.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    public String name, email;
+    public String name, email, points;
     public List<Results> result;
 
-    public User(String name, String email, List<Results> result) {
+    public User() {}
+
+    public User(String name, String email, List<Results> result, String points) {
         this.name = name;
         this.email = email;
         this.result = result;
+        this.points = points;
     }
 
     public String getName() {
@@ -35,5 +38,13 @@ public class User {
 
     public void setResult(List<Results> result) {
         this.result = result;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
     }
 }
